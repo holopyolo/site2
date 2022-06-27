fb888b25e387b59d3d43c2524fb8b12747299dbb
holopyolo <44020572+holopyolo@users.noreply.github.com>
2022-06-27 19:00:47 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,44 @@
-
-document.querySelector('h1').onclick = function() {
-    if(document.querySelector('img').getAttribute('src') === 'images/test.jpg')
+    var temp1 = 1
+    var temp2 = 0
+    var temp3 = 0
+    var myButton = document.querySelector('button');
+    var myHeading = document.querySelector('h1');
+    async function getprice()
     {
-        document.querySelector('img').setAttribute('src', 'images/image.jpg')
+
+        let resp1 = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd")
+        let resp2 = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=near&vs_currencies=usd")
+        let resp3 = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+
+        let f = document.getElementById('sol')
+        let s = document.getElementById('near')
+        let t = document.getElementById('btc')
+
+        if(resp3.ok)
+        {
+            let json1 = await resp1.json()
+            let json2 = await resp2.json()
+            let json3 = await resp3.json()
+            f.innerHTML = 'price SOL: ' + json1["solana"]["usd"] + '  '
+            s.innerHTML = 'price near: ' + json2["near"]["usd"]
+            t.innerHTML = 'price bitcoin: ' + json3["bitcoin"]["usd"]
+        }
+        else
+        {
+            f.innerHTML = 'RATE LIMITED'
+            s.innerHTML = 'RATE LIMITED'
+            t.innerHTML = 'RATE LIMITED'
+            console.log('GG')
+        }
+
     }
-    else
+
+
+
+    myButton.onclick = function()
     {
-        document.querySelector('img').setAttribute('src', 'images/test.jpg')
+        myButton.remove()
+        const inter = setInterval(getprice,3444)
+        getprice()
+
     }
-
-}
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h4');
-function setname()
-{
-
-    var name = prompt('enter ur name')
-    localStorage.setItem('name',name)
-    myHeading.textContext = 'YOO, ' + name
-}
-
-if(!localStorage.getItem('name')) {
-  setname();
-}
-    else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function()
-{
-    setname()
-
-}
